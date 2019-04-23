5803e0f1c1aefced2dd04882bb03005e38d3bfe9
rg3915 <rg3915@yahoo.com.br>
2019-04-23 02:15:18 -0300

    TabularInline. Close #7

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Estoque, EstoqueItens
 
 
+class EstoqueItensInline(admin.TabularInline):
+    model = EstoqueItens
+    extra = 0
+
+
 @admin.register(Estoque)
 class EstoqueAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensInline,)
     list_display = ('__str__', 'nf')
     search_fields = ('nf',)
     list_filter = ('funcionario',)
