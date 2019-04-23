090f251aff6229a27539c7d6330692c8258bd545
rg3915 <rg3915@yahoo.com.br>
2019-04-23 04:24:32 -0300

    Produto: ProdutoUpdate. Close #15

--- projeto/produto/urls.py
+++ projeto/produto/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', v.produto_list, name='produto_list'),
     path('<int:pk>/', v.produto_detail, name='produto_detail'),
     path('add/', v.ProdutoCreate.as_view(), name='produto_add'),
+    path('<int:pk>/edit/', v.ProdutoUpdate.as_view(), name='produto_edit'),
 ]
--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from .models import Produto
 from .forms import ProdutoForm
 
@@ -24,7 +24,12 @@ def produto_add(request):
 
 
 class ProdutoCreate(CreateView):
-    # CreateView
+    model = Produto
+    template_name = 'produto_form.html'
+    form_class = ProdutoForm
+
+
+class ProdutoUpdate(UpdateView):
     model = Produto
     template_name = 'produto_form.html'
     form_class = ProdutoForm
