83b795c8051647f6d7f1715943103a4d4fc11527
rg3915 <rg3915@yahoo.com.br>
2019-04-24 14:55:40 -0300

    Estoque: managers.py. Close #22

--- projeto/estoque/models.py
+++ projeto/estoque/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.urls import reverse_lazy
 from projeto.core.models import TimeStampedModel
 from projeto.produto.models import Produto
+from .managers import EstoqueEntradaManager, EstoqueSaidaManager
 
 
 MOVIMENTO = (
@@ -29,12 +30,6 @@ class Estoque(TimeStampedModel):
         return str(self.nf).zfill(3)
 
 
-class EstoqueEntradaManager(models.Manager):
-
-    def get_queryset(self):
-        return super(EstoqueEntradaManager, self).get_queryset().filter(movimento='e')
-
-
 class EstoqueEntrada(Estoque):
 
     objects = EstoqueEntradaManager()
@@ -43,12 +38,6 @@ class EstoqueEntrada(Estoque):
         proxy = True
         verbose_name = 'estoque entrada'
         verbose_name_plural = 'estoque entrada'
-
-
-class EstoqueSaidaManager(models.Manager):
-
-    def get_queryset(self):
-        return super(EstoqueSaidaManager, self).get_queryset().filter(movimento='s')
 
 
 class EstoqueSaida(Estoque):
