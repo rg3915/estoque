19d69c3317f63bc2f2b6c9b20eddea8a041bbf04
rg3915 <rg3915@yahoo.com.br>
2019-04-24 18:42:34 -0300

    Fazer Logout. Close #37

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import include, path
 
 urlpatterns = [
@@ -21,4 +22,5 @@ urlpatterns = [
     path('produto/', include('projeto.produto.urls')),
     path('estoque/', include('projeto.estoque.urls')),
     path('admin/', admin.site.urls),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
