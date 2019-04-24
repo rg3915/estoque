e5d699ec1a811dcc37190bfceca9d76eb6c785b7
rg3915 <rg3915@yahoo.com.br>
2019-04-24 19:32:43 -0300

    Campo de busca em produtos. Close #43

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -8,6 +8,9 @@ from .forms import ProdutoForm
 def produto_list(request):
     template_name = 'produto_list.html'
     objects = Produto.objects.all()
+    search = request.GET.get('search')
+    if search:
+        objects = objects.filter(produto__icontains=search)
     context = {'object_list': objects}
     return render(request, template_name, context)
 
