8b8ec939d70235454ea8bbd927d64523fa9619b9
rg3915 <rg3915@yahoo.com.br>
2019-04-24 17:25:35 -0300

    Transformando estoque_entrada_detail.html e estoque_saida_detail.html em um só. Close #30

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -18,9 +18,12 @@ def estoque_entrada_list(request):
 
 
 def estoque_entrada_detail(request, pk):
-    template_name = 'estoque_entrada_detail.html'
+    template_name = 'estoque_detail.html'
     obj = EstoqueEntrada.objects.get(pk=pk)
-    context = {'object': obj}
+    context = {
+        'object': obj,
+        'url_list': 'estoque:estoque_entrada_list'
+    }
     return render(request, template_name, context)
 
 
@@ -87,9 +90,12 @@ def estoque_saida_list(request):
 
 
 def estoque_saida_detail(request, pk):
-    template_name = 'estoque_saida_detail.html'
+    template_name = 'estoque_detail.html'
     obj = EstoqueSaida.objects.get(pk=pk)
-    context = {'object': obj}
+    context = {
+        'object': obj,
+        'url_list': 'estoque:estoque_saida_list'
+    }
     return render(request, template_name, context)
 
 
