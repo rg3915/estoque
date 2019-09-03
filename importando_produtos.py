0a8add743de9fefb4df649aadd5a899c7adc28a9
rg3915 <rg3915@yahoo.com.br>
2019-09-03 02:25:10 -0300

    Importando CSV com Django. Close #47

---
+++ importando_produtos.py
@@ -0,0 +1,42 @@
+import csv
+from projeto.produto.models import Produto
+
+
+def csv_to_list(filename: str) -> list:
+    '''
+    Lê um csv e retorna um OrderedDict.
+    Créditos para Rafael Henrique
+    https://bit.ly/2FLDHsH
+    '''
+    with open(filename) as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        csv_data = [line for line in reader]
+    return csv_data
+
+
+def save_data(data):
+    '''
+    Salva os dados no banco.
+    '''
+    aux = []
+    for item in data:
+        produto = item.get('produto')
+        ncm = str(item.get('ncm'))
+        importado = True if item.get('importado') == 'True' else False
+        preco = item.get('preco')
+        estoque = item.get('estoque')
+        estoque_minimo = item.get('estoque_minimo')
+        obj = Produto(
+            produto=produto,
+            ncm=ncm,
+            importado=importado,
+            preco=preco,
+            estoque=estoque,
+            estoque_minimo=estoque_minimo,
+        )
+        aux.append(obj)
+    Produto.objects.bulk_create(aux)
+
+
+data = csv_to_list('fix/produtos.csv')
+save_data(data)
