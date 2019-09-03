5783ebf5af8a7372f3f67e4f805f7549da2ac4ff
rg3915 <rg3915@yahoo.com.br>
2019-09-03 02:26:27 -0300

    Importando CSV InMemoryUploadedFile. Close #48

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,5 +1,9 @@
-from django.http import JsonResponse
+import csv
+import io
+from django.contrib import messages
+from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import CreateView, UpdateView, ListView
 from .models import Produto
 from .forms import ProdutoForm
@@ -50,3 +54,42 @@ def produto_json(request, pk):
     produto = Produto.objects.filter(pk=pk)
     data = [item.to_dict_json() for item in produto]
     return JsonResponse({'data': data})
+
+
+def save_data(data):
+    '''
+    Salva os dados no banco.
+    '''
+    aux = []
+    for item in data:
+        produto = item.get('produto')
+        ncm = str(item.get('ncm'))
+        importado = True if item.get('importado') == 'True' else False
+        preco = item.get('preco')
+        estoque = item.get('estoque')
+        estoque_minimo = item.get('estoque_minimo')
+        obj = Produto(
+            produto=produto,
+            ncm=ncm,
+            importado=importado,
+            preco=preco,
+            estoque=estoque,
+            estoque_minimo=estoque_minimo,
+        )
+        aux.append(obj)
+    Produto.objects.bulk_create(aux)
+
+
+def import_csv(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        # Lendo arquivo InMemoryUploadedFile
+        file = myfile.read().decode('utf-8')
+        reader = csv.DictReader(io.StringIO(file))
+        # Gerando uma list comprehension
+        data = [line for line in reader]
+        save_data(data)
+        return HttpResponseRedirect(reverse('produto:produto_list'))
+
+    template_name = 'produto_import.html'
+    return render(request, template_name)
