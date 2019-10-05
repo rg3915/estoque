7fa226cb71a9667020232aa413e2ba02027ad5ce
rg3915 <rg3915@yahoo.com.br>
2019-10-05 06:08:17 -0300

    Admin: exportando CSV e XLSX. Close #58

--- projeto/core/static/js/estoque_admin.js
+++ projeto/core/static/js/estoque_admin.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
+  $('#changelist-filter').prepend('<h2>Exportar</h2><ul><li><a href="/produto/export/csv/">CSV</a></li><li><a href="/produto/export/xlsx/">XLSX</a></li></ul>')
   $('.object-tools').prepend('<li><a href="/produto/import/csv/">Importar CSV</a></li><li><a href="/produto/import/xlsx/">Importar XLSX</a></li>')
 });
