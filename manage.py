6ecd014271853ffaade03dc7e79c4dd4951c89bb
Danilo Marto de Carvalho <dmcarvalho.dmc@yandex.com>
2022-07-25 08:55:51 -0300

    Movi os arquivos de configuração de projeto de den- tro do pacote projeto para um dedicado      chamado config na raiz do repositório. Também fiz as  devi- das correçõs no projeto para que ele continue fun - cionando corretamente.

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projeto.settings")
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
